8aa6e989b490b814b4610b033b1f3161fe103237
lucaspedronet <lucaspedrolbg@gmail.com>
2019-04-03 18:34:28 -0300

    Add as diretivas [(ngModel)] para nome, sexo, idade e cidade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'questionario-social';
+  nome: string = null;
+  sexo: boolean = null;
+  idade: number = null;
+  cidade: string = null;
+  masculino: boolean = null;
+  feminino: boolean = null;
+
+  questionario_social: Array<object> = [];
+
+  constructor() {}
+
+  salvar() {
+    console.log(this.sexo);
+    const pessoa = {
+      nome: this.nome,
+      sexo: this.sexo,
+      idade: this.idade,
+      cidade: this.cidade
+    };
+
+    this.questionario_social.push(pessoa);
+  }
 }
